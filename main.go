a2979efeaee717d62da73f578118d8626e080a0a
Siddharth Goswami <siddharth.goswami@Triconinfotech.com>
2025-01-28 15:47:24 +0530

    Changes made in main file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import(
 	"context"
 	"fmt"
 	"time"
+	"net/http"
 )
 
 
@@ -16,6 +17,8 @@ func performTask(ctx context.Context, taskname string){
 	}
 }
 
+
+
 func main(){
 	fmt.Println("Starting main...")
 
@@ -26,4 +29,46 @@ func main(){
 
 	time.Sleep(4 * time.Second)
 	fmt.Println("Exiting main...")
+
+
+	http.HandleFunc("/data", dataHandler)
+
+	port := ":8080"
+	fmt.Println("Server listening on port", port)
+
+	if err := http.ListenAndServe(port, nil); err != nil{
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+
+type DataRespone struct{
+	Message string `json:"message"`
+}
+
+func fetchData(ctx context.Context) (string, error){
+	select{
+	case <- time.After(3 * time.Second):
+		return "Data fetched successfully", nil
+	case <- ctx.Done():
+		return "", ctx.Err()
+	}
+}
+
+
+func dataHandler(w http.ResponseWriter, r *http.Request){
+	ctx, cancel := context.WithTimeout(r.Context(), 2 * time.Second)
+	defer cancel()
+
+	_, err := fetchData(ctx)
+	if err != nil{
+		if err == context.DeadlineExceeded{
+			http.Error(w, "Request timeout", http.StatusRequestTimeout)
+		}else{
+			http.Error(w, "Request cancelled", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 }
